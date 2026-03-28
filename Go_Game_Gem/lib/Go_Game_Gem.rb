e6da4f3ed11dd13f8b586c32e615853e017153f0
Tiverkaeva Eva <tiverkaevaeva2005@mail.ru>
2026-03-28 18:23:03 +0300

    Добавила тест для Виселицы(и для этого слегка поменяла код самой игры)

--- Go_Game_Gem/lib/Go_Game_Gem.rb
+++ Go_Game_Gem/lib/Go_Game_Gem.rb
@@ -3,30 +3,30 @@
 require_relative "Go_Game_Gem/version"
 require_relative "Games/hangman"
 require_relative "Games/tictactoe"
-require_relative "Games/Sudoka"
+require_relative "Games/sudoka"
 
 module GoGameGem
   class Error < StandardError; end
-    def self.start
-      puts "Добро пожаловать в мини-игры!"
-      puts "Выберите игру:"
-      puts "1. Виселица"
-      puts "2. Крестики_нолики"
-      puts "3. Судоку"
-      print "Введите номер игры: "
 
-      choice = gets.chomp.to_i
+  def self.start
+    puts "Добро пожаловать в мини-игры!"
+    puts "Выберите игру:"
+    puts "1. Виселица"
+    puts "2. Крестики_нолики"
+    puts "3. Судоку"
+    print "Введите номер игры: "
 
-      case choice
-      when 1
-        Hangman.new
-      when 2
-        TicTacToe.new.start
-      when 3
-        Sudoka.new
-      else
-        puts "Некорректный выбор."
-      end
+    choice = gets.chomp.to_i
+
+    case choice
+    when 1
+      Hangman.new
+    when 2
+      TicTacToe.new.start
+    when 3
+      Sudoka.new
+    else
+      puts "Некорректный выбор."
     end
-
+  end
 end
